3f48e5ca59938b3a4b24e0c697ec1498e3e76d55
David Schmidt <schmidtdavidsimon@gmail.com>
2025-02-16 13:30:18 +0100

    fixed build script playing in foreground

--- BuildDeck.bat
+++ BuildDeck.bat
@@ -3,8 +3,8 @@ batch=BuildDeck.bat
 del Cards\Backs\*.png
 del Cards\Fronts\*.png
 
-"..\nandeck.exe" /exec ActionDeck_Cover_Front.txt
-"..\nandeck.exe" /exec ActionDeck_Cover_Back.txt
-"..\nandeck.exe" /exec ActionDeck_Fronts.txt
-"..\nandeck.exe" /exec ActionDeck_Backs.txt
-"..\nandeck.exe" /exec ActionDeck_Builder.txt
+start /min "" "..\nandeck.exe" /exec ActionDeck_Cover_Back.txt
+start /min "" "..\nandeck.exe" /exec ActionDeck_Cover_Front.txt
+start /min "" "..\nandeck.exe" /exec ActionDeck_Backs.txt
+start /min /wait "" "..\nandeck.exe" /exec ActionDeck_Fronts.txt
+start /min "" "..\nandeck.exe" /exec ActionDeck_Builder.txt
