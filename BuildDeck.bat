21bb701d9ce04bcf8b6a1266deb25dab9062ac0e
David Schmidt <schmidtdavidsimon@gmail.com>
2025-02-16 19:43:37 +0100

    fixed build race issue
    Split up build .bat into 2 files to wait for a batch of commands
    Cleaned up files and adjusted references

--- BuildDeck.bat
+++
@@ -1,9 +0,0 @@
-del Cards\Backs\*.png
-del Cards\Fronts\*.png
-
-start /min "" "..\nandeck.exe" /exec ActionDeck_Cover_Back.txt
-start /min "" "..\nandeck.exe" /exec ActionDeck_Cover_Front.txt
-start /min "" "..\nandeck.exe" /exec ActionDeck_Backs.txt
-start /min /wait "" "..\nandeck.exe" /exec ActionDeck_Fronts.txt
-start /min /wait "" "..\nandeck.exe" /exec ActionDeck_Builder.txt
-del *.html
---
+++ BuildPipeline/GenerateCards.bat
@@ -0,0 +1,4 @@
+start /min "" "..\..\nandeck.exe" /exec ..\nanDECKScripts\ActionDeck_Cover_Front.txt
+start /min "" "..\..\nandeck.exe" /exec ..\nanDECKScripts\ActionDeck_Cover_Back.txt
+start /min "" "..\..\nandeck.exe" /exec ..\nanDECKScripts\ActionDeck_Fronts.txt
+start /min "" "..\..\nandeck.exe" /exec ..\nanDECKScripts\ActionDeck_Backs.txt
