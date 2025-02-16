456938b53479baf59dc918165b0ada5636d08348
David Schmidt <schmidtdavidsimon@gmail.com>
2025-02-16 16:58:18 +0100

    deleting left over temp files after build

--- BuildDeck.bat
+++ BuildDeck.bat
@@ -1,5 +1,3 @@
-batch=BuildDeck.bat
-
 del Cards\Backs\*.png
 del Cards\Fronts\*.png
 
@@ -7,4 +5,5 @@ start /min "" "..\nandeck.exe" /exec ActionDeck_Cover_Back.txt
 start /min "" "..\nandeck.exe" /exec ActionDeck_Cover_Front.txt
 start /min "" "..\nandeck.exe" /exec ActionDeck_Backs.txt
 start /min /wait "" "..\nandeck.exe" /exec ActionDeck_Fronts.txt
-start /min "" "..\nandeck.exe" /exec ActionDeck_Builder.txt
+start /min /wait "" "..\nandeck.exe" /exec ActionDeck_Builder.txt
+del *.html
